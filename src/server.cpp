5d88cbebf96bbba335376d1240e23c7f36743454
mariano-git <caminosmariano1@gmail.com>
2025-04-18 13:29:59 -0300

    fixed libraries causing errors, changes on CMakeLists.txt, [added server.exe]

--- src/server.cpp
+++ src/server.cpp
@@ -2,14 +2,14 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
-#include <unistd.h>
 #include <sys/types.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 int main(int argc, char **argv) {
 
-  // Initialize Winsock
+  // [1] Initialize Winsock
+
   WSADATA wsaData;
   if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
     std::cerr << "WSAStartup failed\n";
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
+  // [2] Create a socket
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -35,11 +35,17 @@ int main(int argc, char **argv) {
 
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
+
+  // [3] Reuse the address
+
   int reuse = 1;
+
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
+
+  // [4] Bind the socket to the port
 
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
@@ -56,7 +62,10 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // [5] Listen for incoming connections
+
   int connection_backlog = 5;
+
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     closesocket(server_fd);
@@ -64,13 +73,15 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  std::cout << "Server listening on port 4221...\n";
+
+  // [6] Accept incoming connections
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
 
-  std::cout << "Waiting for a client to connect...\n";
-
   // Store the value returned by accept in a variable
-  auto client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  auto client = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 
   if (client == INVALID_SOCKET) {
     std::cerr << "accept failed\n";
@@ -79,48 +90,53 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  std::cout << "Client connected\n";
+
+  // [7] Receive data from the client
+
   // Create a buffer to store the client's message
   char buffer[1024] = {0};
-  char path[512] = {0};
-
-  std::cout << "Client connected\n";
 
   int bytes_received = recv(client, buffer, sizeof(buffer), 0);
 
-  // Extract the path 
-
-  // strtok tokenizes the string buffer by the delimiter " " and returns a pointer to the first token (in this case the path)
-  char* m_Path = strtok(buffer, " "); 
-  m_Path = strtok(NULL, " ");
-
-  // This will be used to store the number of bytes sent
-  int bytes_sent;
-
-  // Send the response back to the client according to the path
-  if (strcmp(m_Path, "/") == 0) 
-  {
-    std::string message = "HTTP/1.1 200 OK\r\n\r\n";
-
-    bytes_sent = send(client, message.c_str(), message.length(), 0);
-  } 
-  else
-  {
-    std::string message = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-    bytes_sent = send(client, message.c_str(), message.length(), 0);
-  }
-
-  if (bytes_sent < 0)
-  {
-    std::cerr << "Failed to send response\n";
+  if (bytes_received <= 0) {
+    std::cerr << "Failed to receive data or client disconnected\n";
     closesocket(client);
     closesocket(server_fd);
     WSACleanup();
     return 1;
+  } else 
+  {
+    buffer[bytes_received] = '\0'; // Null-terminate the received data
+
+    // Extract the path parsing the string buffer
+
+    std::string req = buffer;
+
+    auto pos = req.find(' ');
+    auto pos2 = req.find(' ', pos + 1);
+
+    std::string path = req.substr(pos + 1, pos2 - pos - 1);
+
+    // [8] Send the response back
+
+    std::string response;
+
+    if (path == "/") {
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else
+    {
+      response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
+    send(client, response.c_str(), response.length(), 0);
   }
 
-  close(client);
-  close(server_fd);
+  // [9] Close connections
+  
+  closesocket(client);
+  closesocket(server_fd);
   WSACleanup();
 
   return 0;
