77208822a84afcfe76b8e83e619052612af7d56b
mariano-git <caminosmariano1@gmail.com>
2025-04-17 21:38:08 -0300

    replaced the linux libraries with windows specify libraries [changes on server.cpp]

--- src/server.cpp
+++ src/server.cpp
@@ -4,11 +4,18 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 int main(int argc, char **argv) {
+
+  // Initialize Winsock
+  WSADATA wsaData;
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    std::cerr << "WSAStartup failed\n";
+    return 1;
+  }
+
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -19,15 +26,17 @@ int main(int argc, char **argv) {
   // Uncomment this block to pass the first stage
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
   if (server_fd < 0) {
-  std::cerr << "Failed to create server socket\n";
-  return 1;
+    std::cerr << "Failed to create server socket\n";
+    WSACleanup();
+    return 1;
   }
 
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
@@ -37,14 +46,21 @@ int main(int argc, char **argv) {
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
 
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+  if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+
+    // The bind function returns SOCKET_ERROR on failure, not -1 like in Linux
+
     std::cerr << "Failed to bind to port 4221\n";
+    closesocket(server_fd);
+    WSACleanup();
     return 1;
   }
 
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
+    closesocket(server_fd);
+    WSACleanup();
     return 1;
   }
 
@@ -56,6 +72,13 @@ int main(int argc, char **argv) {
   // Store the value returned by accept in a variable
   auto client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
+  if (client == INVALID_SOCKET) {
+    std::cerr << "accept failed\n";
+    closesocket(server_fd);
+    WSACleanup();
+    return 1;
+  }
+
   // Create a buffer to store the client's message
   char buffer[1024] = {0};
   char path[512] = {0};
@@ -65,8 +88,10 @@ int main(int argc, char **argv) {
   int bytes_received = recv(client, buffer, sizeof(buffer), 0);
 
   // Extract the path 
-  char* m_Path = strtok(buffer, " ");
-  m_Path = strtok(NULL, " "); 
+
+  // strtok tokenizes the string buffer by the delimiter " " and returns a pointer to the first token (in this case the path)
+  char* m_Path = strtok(buffer, " "); 
+  m_Path = strtok(NULL, " ");
 
   // This will be used to store the number of bytes sent
   int bytes_sent;
@@ -88,11 +113,15 @@ int main(int argc, char **argv) {
   if (bytes_sent < 0)
   {
     std::cerr << "Failed to send response\n";
+    closesocket(client);
+    closesocket(server_fd);
+    WSACleanup();
     return 1;
   }
 
   close(client);
   close(server_fd);
+  WSACleanup();
 
   return 0;
 }
